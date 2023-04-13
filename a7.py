842d111f46ebcab30c48ce736df6c53e1f141233
Disguised-Coffee <danielidanque@gmail.com>
2023-04-13 10:13:37 -0500

    added a7.py

---
+++ a7.py
@@ -0,0 +1,20 @@
+#interpreter command -->$ python 3
+
+from neural import NeuralNet
+
+xor_data = [
+    ([0,0],[0]),
+    ([0,1],[1]),
+    ([1,0],[1]),
+    ([1,1],[1]),]
+
+or_data = [
+    ([0,0],[0]),
+    ([0,1],[1]),
+    ([1,0],[1]),
+    ([1,1],[0]),]
+
+orn = NeuralNet(2,3,1)
+orn.train(xor_data)
+
+print(orn.test_with_expected(xor_data))
